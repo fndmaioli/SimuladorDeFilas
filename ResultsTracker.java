3d09d864cc44d56dd85656725dad156b042d2da2
Fernando Locatelli Maioli <fndmaioli5@gmail.com>
2020-10-20 19:39:19 -0300

    calcula e apresenta os indices de desempenho de cada fila

--- ResultsTracker.java
+++ ResultsTracker.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class ResultsTracker {
 
-    public HashMap<String, HashMap<Integer, Double>> queuesResult = new HashMap<String, HashMap<Integer, Double>>();
+    public HashMap<String, Queue> queuesResult = new HashMap<String, Queue>();
     public Double time;
     public int numberOfSimulations = 0;
 
@@ -17,17 +17,17 @@ public class ResultsTracker {
 
                 for(Integer stateKey : newResults.get(queueKey).queueStates.keySet()) {
                     Double newVal = newResults.get(queueKey).queueStates.get(stateKey);
-                    if (queuesResult.get(queueKey).containsKey(stateKey)) {
-                        newVal += queuesResult.get(queueKey).get(stateKey);
-                        queuesResult.get(queueKey).replace(stateKey, newVal);
+                    if (queuesResult.get(queueKey).queueStates.containsKey(stateKey)) {
+                        newVal += queuesResult.get(queueKey).queueStates.get(stateKey);
+                        queuesResult.get(queueKey).queueStates.replace(stateKey, newVal);
                     } 
                     else {
-                        queuesResult.get(queueKey).put(stateKey, newVal);
+                        queuesResult.get(queueKey).queueStates.put(stateKey, newVal);
                     }
                 }
             } 
             else {
-                queuesResult.put(queueKey, newResults.get(queueKey).queueStates);
+                queuesResult.put(queueKey, newResults.get(queueKey));
             }
         }
     }
@@ -42,17 +42,60 @@ public class ResultsTracker {
         System.out.println("Tempo total: " + this.time/5);
         for(String keyName : queuesResult.keySet()) {
             System.out.println("Probabilidade de cada estado da fila: " + keyName);
-            for(Integer key : queuesResult.get(keyName).keySet()) {
+            for(Integer key : queuesResult.get(keyName).queueStates.keySet()) {
                 if (key >= 0) {
-                    System.out.println(key + "   -   tempo: " + new DecimalFormat("#.##").format(queuesResult.get(keyName).get(key)/this.numberOfSimulations) + "   -   " + new DecimalFormat("#.##").format((queuesResult.get(keyName).get(key) * 100 )/this.time) + "%");
+                    System.out.println(key + "   -   tempo: " + new DecimalFormat("#.##").format(queuesResult.get(keyName).queueStates.get(key)/this.numberOfSimulations) + "   -   " + new DecimalFormat("#.##").format((queuesResult.get(keyName).queueStates.get(key) * 100)/this.time) + "%");
                 }
             }
-            if (queuesResult.get(keyName).containsKey(-1)) {
-                System.out.println("Numero de perdas: " + (int) (queuesResult.get(keyName).get(-1)/this.numberOfSimulations));
+            if (queuesResult.get(keyName).queueStates.containsKey(-1)) {
+                System.out.println("Numero de perdas: " + (int) (queuesResult.get(keyName).queueStates.get(-1)/this.numberOfSimulations));
             } else {
                 System.out.println("Numero de perdas: " + 0);
             }
+            double ap = averagePopulation(queuesResult.get(keyName));
+            double fr = flowRate(queuesResult.get(keyName));
+            utilization(queuesResult.get(keyName));
+            responseTime(ap, fr);
         }
+    }
 
+    public double averagePopulation(Queue queue) {
+        double sum = 0.0;
+        for(Integer key : queue.queueStates.keySet()) {
+            if (key == 0 || key == -1) { continue; }
+            sum += key * (queue.queueStates.get(key)/this.time);
+        }
+        System.out.println("População média: " + new DecimalFormat("#.##").format(sum));
+        return sum;
+    }
+
+    public double flowRate(Queue queue) {
+        double sum = 0.0;
+        for(Integer key : queue.queueStates.keySet()) {
+            if (key == 0 || key == -1) { continue; }
+            sum += (queue.queueStates.get(key)/this.time) * ((queue.maxService + queue.minService)/2);
+        }
+        System.out.println("Vazão: " + new DecimalFormat("#.##").format(sum));
+        return sum;
+    }
+
+    public void utilization(Queue queue) {
+        double sum = 0.0;
+        for(Integer key : queue.queueStates.keySet()) {
+            if (key == 0 || key == -1) { continue; }
+            sum += (queue.queueStates.get(key)/this.time) * (getSmallerValue(key, (int) queue.capacity)/(int) queue.capacity);
+        }
+        System.out.println("Utilização: " + new DecimalFormat("#.##").format(sum));
+    }
+
+    public void responseTime(double averagePopulation, double flowRate) {
+        System.out.println("Tempo de resposta: " + new DecimalFormat("#.##").format(averagePopulation/flowRate));
+    }
+
+    public int getSmallerValue(int val1, int val2) {
+        if(val1 < val2) {
+            return val1;
+        }
+        return val2;
     }
 }
